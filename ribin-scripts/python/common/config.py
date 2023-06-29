b1166cc87dbc1ddef9b4c8d8aa6cea38bf724c4e
bud-caolibin <caolibin@budcreate.io>
2023-06-29 18:29:07 +0800

    feat:script/python

---
+++ ribin-scripts/python/common/config.py
@@ -0,0 +1,45 @@
+from common.singleton import Singleton
+from typing import Optional, Dict
+
+
+class ServiceConfig:
+    openai_api: str = ""
+    env: str = ""
+    redis_addr: str = ""
+    redis_username: str = ""
+    redis_password: str = ""
+
+
+class LogConfig:
+    log_level: str = ""
+    log_path: str = ""
+    log_mode: str = ""
+
+
+class GlobalConfig(Singleton):
+    def __init__(self):
+        self.service_config: Optional[ServiceConfig] = None
+        self.log_config: Optional[LogConfig] = None
+
+    def load_config(self, path: str = "./config.yaml"):
+        from common.utils import data_util
+
+        data: Dict[str, Dict[str, str]] = data_util.load_from_yaml(path)
+        for k, d in data.items():
+            if k == "ServiceConfig":
+                if not self.service_config:
+                    self.service_config = ServiceConfig()
+                self.service_config.__dict__.update(d)
+            elif k == "LogConfig":
+                if not self.log_config:
+                    self.log_config = LogConfig()
+                self.log_config.__dict__.update(d)
+
+
+global_config = GlobalConfig()
+
+
+if __name__ == "__main__":
+    global_config.load_config("../config.yaml")
+    print(global_config.service_config.openai_api)
+    print(global_config.log_config.log_path)
---
+++ ribin-scripts/python/common/utils.py
@@ -0,0 +1,26 @@
+from common.singleton import Singleton
+from typing import Optional
+
+
+class DataUtil(Singleton):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def load_from_yaml(path: str) -> Optional[dict]:
+        with open(path, "r", encoding="utf-8") as f:
+            import yaml
+
+            data = yaml.safe_load(f)
+        return data
+
+    @staticmethod
+    def load_from_json(path: str) -> Optional[dict]:
+        with open(path, "r", encoding="utf-8") as f:
+            import json
+
+            data = json.load(f)
+        return data
+
+
+data_util = DataUtil()
