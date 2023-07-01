4f97713dd608f84c9eda71c21e4c671904c21a24
ribincao <njucaolibin@163.com>
2023-07-01 19:55:39 +0800

    format:调整代码结构

--- ribin-scripts/python/aigc/chatgpt_app.py
+++ ribin-scripts/python/aigc/chatgpt_app.py
@@ -3,24 +3,16 @@ from common.utils import aprint
 from langchain import OpenAI, ConversationChain, LLMChain, PromptTemplate
 from langchain.memory import ConversationBufferWindowMemory
 import os
+from aigc.prompts import chatgpt_prompt, linux_prompt
 
 os.environ["OPENAI_API_KEY"] = get_opeai_api_key()
 os.environ["SERPAPI_API_KEY"] = get_serp_api_key()
 
-template = """Assistant is a large language model trained by OpenAI.
-Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
-Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
-Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
-{history}
-Human: {human_input}
-Assistant:"""
-
-context_example = "I want you to act as a Linux terminal. I will type commands and you will reply with what the terminal should show. I want you to only reply with the terminal output inside one unique code block, and nothing else. Do not write explanations. Do not type commands unless I instruct you to do so. When I need to tell you something in English I will do so by putting text inside curly brackets {like this}. My first command is pwd."
 
 
-def chatgpt(init_context: str = context_example) -> LLMChain:
+def chatgpt(init_context: str = linux_prompt) -> LLMChain:
     prompt = PromptTemplate(
-        input_variables=["history", "human_input"], template=template
+        input_variables=["history", "human_input"], template=chatgpt_prompt
     )
     chatgpt_chain = LLMChain(
         llm=OpenAI(temperature=0.0),  # type: ignore
@@ -40,12 +32,9 @@ def chatgpt(init_context: str = context_example) -> LLMChain:
 
 
 def linux_shell():
-    chatgpt(context_example)
-
-
-def eight_app():
-    chatgpt("我想让你扮演一个精通八卦、占星的算命大师。我将告诉你出生年月，我希望你能帮我排一下生辰八字.")
+    chatgpt(linux_prompt)
 
 
 if __name__ == "__main__":
-    eight_app()
+    linux_shell()
+
